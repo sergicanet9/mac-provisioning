fe615af5cdf58561dff10b28c5b24fa467edfa1a
Sergi Canet <sergi@sergicanet.com>
2025-12-02 17:51:27 +0100

    updates

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-/bin/bash -c "$(curl -sL https://raw.githubusercontent.com/sergicanet9/mac-provisioning/main/setup.sh)"
+install/update mac-provisioning: /bin/bash -c "$(curl -sL https://raw.githubusercontent.com/sergicanet9/mac-provisioning/main/setup.sh)"
+
+dump homebrew + go packages: brew bundle dump --describe --force --no-vscode
+install homebrew + go packages: brew bundle install --file=Brewfile
--- setup.sh
+++ setup.sh
@@ -4,6 +4,9 @@
 # Installs or updates mac-provisioning on a Mac.
 # ==================================================
 
+# ===========================================
+# 1. Check if mac-provisioning is installed
+# ===========================================
 VERSION_FILE="$HOME/.mac_provisioning_version"
 VERSION_URL="https://raw.githubusercontent.com/sergicanet9/mac-provisioning/main/VERSION"
 
@@ -14,7 +17,7 @@ if [ -z "$LATEST_VERSION" ]; then
 fi
 
 if [ ! -f "$VERSION_FILE" ]; then
-    echo "mac-provisioning is not installed. Proceeding with installation..."
+    echo "mac-provisioning not found. Installing..."
     NEW_INSTALLATION=true
 else
     INSTALLED_VERSION=$(cat "$VERSION_FILE")
@@ -28,5 +31,49 @@ else
     fi
 fi
 
+# TODO dotfiles
+
+# ===========================================
+# 2. Install or update Homebrew
+# ===========================================
+if ! command -v brew &> /dev/null; then
+    echo "Homebrew not found. Installing..."
+    NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    echo "Homebrew already installed. Updating..."
+    brew update
+fi
+
+# ===========================================
+# 3. Apply Brewfile for packages and casks
+# ===========================================
+BREWFILE_TMP="/tmp/Brewfile"
+
+curl -sL https://raw.githubusercontent.com/sergicanet9/mac-provisioning/main/homebrew/Brewfile -o "$BREWFILE_TMP"
+
+if [ ! -s "$BREWFILE_TMP" ]; then
+    echo "Failed to download Brewfile"
+    exit 1
+fi
+
+brew bundle install --file="$BREWFILE_TMP"
+
+# ===========================================
+# 4. Apply Brewfile for Go packages
+# ===========================================
+BREWFILE_TMP_GO="/tmp/Brewfile"
+
+curl -sL https://raw.githubusercontent.com/sergicanet9/mac-provisioning/main/go/Brewfile -o "$BREWFILE_TMP_GO"
+
+if [ ! -s "$BREWFILE_TMP_GO" ]; then
+    echo "Failed to download Brewfile"
+    exit 1
+fi
+
+brew bundle install --file="$BREWFILE_TMP_GO"
+
+# ===========================================
+# X. Set installed version
+# ===========================================
 echo "$LATEST_VERSION" > "$VERSION_FILE"
 echo "✅ mac-provisioning $LATEST_VERSION installed"
