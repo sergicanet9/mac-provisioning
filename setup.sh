decc2d782f6d3da3f8347d8e55d3b6504b240781
Sergi Canet <sergi.canet@derivco.se>
2025-12-03 13:44:27 +0100

    changes

--- setup.sh
+++ setup.sh
@@ -5,7 +5,33 @@
 # ==================================================
 
 # ===========================================
-# 1. Check if mac-provisioning is installed
+# Variables & Helper functions
+# ===========================================
+FILES_BASE="https://raw.githubusercontent.com/sergicanet9/mac-provisioning/main"
+TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
+
+declare new_installation
+
+backup_file() {
+    local file="$1"
+    if [ -f "$file" ] && [ "$new_installation" = false ]; then
+        echo "Backup existing $(basename "$file")"
+        mv "$file" "${file}.backup_$TIMESTAMP"
+    fi
+}
+
+install_file() {
+    local filename="$1"
+    local target="$2"
+
+    backup_file "$target"
+
+    echo "Installing $filename"
+    curl -fsSL "$FILES_BASE/$filename" -o "$target"
+}
+
+# ===========================================
+echo "1. Check if mac-provisioning is installed"
 # ===========================================
 VERSION_FILE="$HOME/.mac_provisioning_version"
 VERSION_URL="https://raw.githubusercontent.com/sergicanet9/mac-provisioning/main/VERSION"
@@ -18,7 +44,7 @@ fi
 
 if [ ! -f "$VERSION_FILE" ]; then
     echo "mac-provisioning not found. Installing..."
-    NEW_INSTALLATION=true
+    new_installation=true
 else
     INSTALLED_VERSION=$(cat "$VERSION_FILE")
 
@@ -27,40 +53,19 @@ else
         exit 0
     else
         echo "mac-provisioning $INSTALLED_VERSION already installed. Updating to $LATEST_VERSION..."
-        NEW_INSTALLATION=false
+        new_installation=false
     fi
 fi
 
 # ===========================================
-# 2. Install dotfiles
+echo "2. Install dotfiles"
 # ===========================================
-timestamp=$(date +"%Y%m%d_%H%M%S")
-FILES_BASE="https://raw.githubusercontent.com/sergicanet9/mac-provisioning/main/dotfiles"
-
-backup_file() {
-    local file="$1"
-    if [ -f "$file" ] && [ "$NEW_INSTALLATION" = false ]; then
-        echo "Backup existing $(basename "$file")"
-        mv "$file" "${file}.backup_$timestamp"
-    fi
-}
-
-install_dotfile() {
-    local filename="$1"
-    local target="$HOME/$filename"
-
-    backup_file "$target"
-
-    echo "Installing $filename"
-    curl -fsSL "$FILES_BASE/$filename" -o "$target"
-}
-
-install_dotfile ".zshrc"
-install_dotfile ".zshcustom"
-install_dotfile ".gitignore_global"
+install_file "dotfiles/.zshrc" "$HOME/.zshrc"
+install_file "dotfiles/.zshcustom" "$HOME/.zshcustom"
+install_file "dotfiles/.gitignore_global" "$HOME/.gitignore_global"
 
 # ===========================================
-# 3. Install or update Oh My Zsh
+echo "3. Install or update Oh My Zsh"
 # ===========================================
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
     echo "Oh My Zsh not found. Installing..."
@@ -71,7 +76,7 @@ else
 fi
 
 # ===========================================
-# 4. Install or update Homebrew
+echo "4. Install or update Homebrew"
 # ===========================================
 if ! command -v brew &> /dev/null; then
     echo "Homebrew not found. Installing..."
@@ -82,7 +87,7 @@ else
 fi
 
 # ===========================================
-# 5. Apply Brewfile for packages and casks
+echo "5. Apply Brewfile for packages and casks"
 # ===========================================
 BREWFILE_TMP="/tmp/Brewfile"
 
@@ -93,9 +98,9 @@ if [ ! -s "$BREWFILE_TMP" ]; then
     exit 1
 fi
 
-if [ "$NEW_INSTALLATION" = false ]; then
+if [ "$new_installation" = false ]; then
     echo "Backing up Brewfile for packages and casks..."
-    brew bundle dump --describe --force --no-vscode --file="/$HOME/.Brewfile_backup_$timestamp"
+    brew bundle dump --describe --force --no-vscode --file="/$HOME/.Brewfile_backup_$TIMESTAMP"
 fi
 
 brew bundle install --file="$BREWFILE_TMP"
@@ -104,7 +109,7 @@ brew bundle install --file="$BREWFILE_TMP"
 # TODO vscode login?
 
 # ===========================================
-# 6. Apply Brewfile for Go packages
+echo "6. Apply Brewfile for Go packages"
 # ===========================================
 BREWFILE_TMP_GO="/tmp/Brewfile_go"
 
@@ -115,15 +120,15 @@ if [ ! -s "$BREWFILE_TMP_GO" ]; then
     exit 1
 fi
 
-if [ "$NEW_INSTALLATION" = false ]; then
+if [ "$new_installation" = false ]; then
     echo "Backing up Brewfile for Go packages ..."
-    brew bundle dump --describe --force --go --file="/$HOME/.Brewfile_go_backup_$timestamp"
+    brew bundle dump --describe --force --go --file="/$HOME/.Brewfile_go_backup_$TIMESTAMP"
 fi
 
 brew bundle install --file="$BREWFILE_TMP_GO"
 
 # ===========================================
-# 7. Configure git and clone repos
+echo "7. Configure git and clone repos"
 # ===========================================
 read -rp "Enter your Git user name: " GIT_USER_NAME
 git config --global user.name "$GIT_USER_NAME"
@@ -145,7 +150,15 @@ for repo in $(gh repo list "$GITHUB_USER" --limit 200 --json name,sshUrl -q '.[]
 done
 
 # ===========================================
-# 8. Set installed version
+echo "8. Set up macOS"
+# ===========================================
+defaults write com.apple.menuextra.clock ShowSeconds -bool true
+install_file "$PLISTS_BASE" "com.apple.dock.plist" "$HOME/Library/Preferences/com.apple.dock.plist"
+install_file "$PLISTS_BASE" "com.apple.finder.plist" "$HOME/Library/Preferences/com.apple.finder.plist"
+
+
+# ===========================================
+echo "9. Set installed version"
 # ===========================================
 echo "$LATEST_VERSION" > "$VERSION_FILE"
 echo "✅ mac-provisioning $LATEST_VERSION installed"
