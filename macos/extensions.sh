56f5173136ae5980eaafc64285ded71cfde940e8
Sergi Canet <sergi.canet@derivco.se>
2025-12-05 12:31:00 +0100

    fix safari extensions

--- macos/extensions.sh
+++ macos/extensions.sh
@@ -3,17 +3,18 @@
 # extensions.sh - Asks to install Safari extensions
 # ==================================================
 
-declare -A extensions=(
-    ["AdBlock"]="1402042596"
-    ["OnePassword"]="1569813296"
+extensions=(
+    "AdBlock|1402042596"
+    "1Password|1569813296"
 )
 
-for name in "${!extensions[@]}"; do
-    id="${extensions[$name]}"
+for ext in "${extensions[@]}"; do
+    name="${ext%%|*}"
+    id="${ext##*|}"
     read -rp "Do you want to install $name (App Store ID: $id)? (y/n) " confirm
     if [[ "$confirm" =~ ^[Yy]$ ]]; then
         open "macappstore://itunes.apple.com/app/id$id"
-        echo "Please complete the installation of $name manually in App Store."
+        echo "Complete the installation of $name manually in App Store."
     else
         echo "Skipping $name."
     fi
