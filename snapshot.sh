100300f0b0722358dbcd6bc36cd47c1c2e00db78
Sergi Canet <sergi.canet@derivco.se>
2025-12-05 21:47:41 +0100

    snapshot

---
+++ snapshot.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+# ==================================================
+# mac-provisioning - snapshot.sh
+# Pulls local machine files into the cloned repo files for diffing.
+# It overrides the corresponding profile files based on the
+# installed profile so that changes can be inspected with git diff.
+# ==================================================
+
+# ===========================================
+# Variables
+# ===========================================
+LOCAL_DIR="$(cd "$(dirname "$0")" && pwd)"
+declare profile
+
+# ===========================================
+echo "1. Check installed profile"
+# ===========================================
+profile_file="$HOME/.mac_provisioning/profile"
+if [[ ! -f "$profile_file" ]]; then
+    echo "No profile found at $profile_file. Make sure that mac-provisioning is installed."
+    exit 1
+fi
+
+profile=$(cat "$profile_file")
+echo "Taking local machine snapshot for profile: $profile"
+
+# ==================================================
+echo "1. Update Brewfile for packages and casks"
+# ==================================================
+target_brewfile="$LOCAL_DIR/homebrew/$profile/Brewfile"
+
+echo "Generating Brewfile for packages and casks..."
+brew bundle dump --describe --force --no-vscode --no-go --file="$target_brewfile"
+echo "Updated Brewfile at: $target_brewfile"
+
+# ==================================================
+echo "2. Update Brewfile for Go packages"
+# ==================================================
+target_brewfile_go="$LOCAL_DIR/go/Brewfile"
+
+echo "Generating Brewfile for Go packages..."
+brew bundle dump --describe --force --go --file="$target_brewfile_go"
+echo "Updated Brewfile at: $target_brewfile_go"
+
+# ==================================================
+echo "3. Update dotfiles"
+# ==================================================
+source_zshrc="$HOME/.zshrc"
+target_zshrc="$LOCAL_DIR/dotfiles/$profile/.zshrc"
+
+if [[ ! -f "$source_zshrc" ]]; then
+    echo "No ~/.zshrc found on the system"
+    exit 1
+else
+    echo "Updating $target_zshrc from local ~/.zshrc"
+    cp "$source_zshrc" "$target_zshrc"
+fi
+
+# ==================================================
+echo "DONE: Local machine snapshot taken. Use git diff to review changes."
+# ==================================================
